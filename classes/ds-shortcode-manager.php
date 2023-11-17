bfeb1e57153e5a7ad0cdee6e5c82605a16de557a
Matt Sadowski <github@yourmatt.com>
2023-11-16 21:53:49 -0700

    Changed shortcode output structure

--- classes/ds-shortcode-manager.php
+++ classes/ds-shortcode-manager.php
@@ -16,11 +16,15 @@ class ds_shortcode_manager {
         foreach ($staff_items as $staff_item) {
 
             // add the staff item
-            $staff_list .= '<p>' . $staff_item->thumb . '</p>';
+            $staff_list .= '<div class="staff-member">';
+            $staff_list .= '<div class="thumb">' . $staff_item->thumb . '</div>';
+            $staff_list .= '<div class="bio">';
             $staff_list .= '<h3>' . $staff_item->post_title . '</h3>';
             if ($staff_item->meta['_ds_title'])
-                $staff_list .= '<p><strong>' . $staff_item->meta['_ds_title'][0] . '</strong></p>';
+                $staff_list .= '<h4>' . $staff_item->meta['_ds_title'][0] . '</h4>';
             $staff_list .= '<p>' . $staff_item->post_content . '</p>';
+            $staff_list .= '</div>';
+            $staff_list .= '</div>';
 
         }
 
