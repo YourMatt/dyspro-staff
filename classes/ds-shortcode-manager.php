7655013bad26b6d43692b7d9bfd96fc31f352c54
Matt Sadowski <github@yourmatt.com>
2026-08-18 22:24:10 -0700

    Added clerical fields to shortcode display

--- classes/ds-shortcode-manager.php
+++ classes/ds-shortcode-manager.php
@@ -15,14 +15,29 @@ class ds_shortcode_manager {
         // display all staff items
         foreach ($staff_items as $staff_item) {
 
+            $title = (isset ($staff_item->meta['_ds_title'][0]) ? $staff_item->meta['_ds_title'][0] : '');
+            $start_date = (isset ($staff_item->meta['_ds_start_date'][0]) ? $staff_item->meta['_ds_start_date'][0] : '');
+            $company = (isset ($staff_item->meta['_ds_company'][0]) ? $staff_item->meta['_ds_company'][0] : '');
+            $committees = (isset ($staff_item->meta['_ds_committee'][0]) ? $staff_item->meta['_ds_committee'][0] : '');
+
+            // everyone is a board member, so only call it out for those without another title
+            $title_suffix = '';
+            if ($start_date) {
+                $title_suffix = ($title == 'Board Member' ? '' : '- Board Member ') . 'since ' . $start_date;
+            }
+
             // add the staff item
             $staff_list .= '<div class="staff-member">';
             $staff_list .= '<div class="thumb">' . $staff_item->thumb . '</div>';
             $staff_list .= '<div class="bio">';
             $staff_list .= '<h3>' . $staff_item->post_title . '</h3>';
-            if ($staff_item->meta['_ds_title'])
-                $staff_list .= '<h4>' . $staff_item->meta['_ds_title'][0] . '</h4>';
-            $staff_list .= '<p>' . $staff_item->post_content . '</p>';
+            if ($company)
+                $staff_list .= '<h4>' . $company . '</h4>';
+            if ($title)
+                $staff_list .= '<h4>' . trim ($title . ' ' . $title_suffix) . '</h4>';
+            if ($committees)
+                $staff_list .= '<h4>' . $committees . ' Committee</h4>';
+            $staff_list .= '<p>' . nl2br ($staff_item->post_content) . '</p>';
             $staff_list .= '</div>';
             $staff_list .= '</div>';
 
--- classes/ds-utilities.php
+++ classes/ds-utilities.php
@@ -2,6 +2,15 @@
 
 class ds_utilities {
 
+    // staff titles listed in the order they should display, everything else follows
+    private static $title_sort_order = array (
+        'chair',
+        'immediate past',
+        'treasurer',
+        'secretary',
+        'board member'
+    );
+
     public static function save_meta_field ($post_id, $field_name, $meta_name) {
 
         $current_value = get_post_meta ($post_id, $meta_name, true);
@@ -42,7 +51,25 @@ class ds_utilities {
             $staff[$i]->thumb = get_the_post_thumbnail ($staff[$i]->ID, 'large');
         }
 
+        // sort by title, then by name within each title
+        usort ($staff, function ($staff_item1, $staff_item2) {
+            $order = self::get_title_sort_position ($staff_item1) - self::get_title_sort_position ($staff_item2);
+            if ($order) return $order;
+            return strcasecmp ($staff_item1->post_title, $staff_item2->post_title);
+        });
+
         return $staff;
+
+    }
+
+    private static function get_title_sort_position ($staff_item) {
+
+        $title = (isset ($staff_item->meta['_ds_title'][0]) ? strtolower (trim ($staff_item->meta['_ds_title'][0])) : '');
+
+        $position = array_search ($title, self::$title_sort_order, true);
+
+        // titles not in the list sort after all the ones that are
+        return ($position === false ? count (self::$title_sort_order) : $position);
 
     }
 
